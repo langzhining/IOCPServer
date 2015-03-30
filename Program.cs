a723fe38e26c99067ea5d4e319baf6e3d2b7ab00
JeffChan <nieyaoyanJune@gmail.com>
2015-03-30 14:49:19 +0800

    Initial Prject
    初始工程

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using IOCPServer;
+
+namespace IOCPServer
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+
+            IOCPServer server = new IOCPServer(8088, 1024, @"C:\Users\hwq\Desktop\IOCPServer\IOCPServer\webapp");
+            server.Start();
+            Console.WriteLine("服务器已启动....");
+            System.Console.ReadLine();
+        }
+    }
+}
